5e36962da436cf437c2dfe370e2161a277c5464b
paulotassi1 <133156624+paulotassi@users.noreply.github.com>
2026-02-20 17:24:37 -0500

    Updated GM and IM

---
+++ Prototypes/SpectaculAR_Soup/Assets/Scripts/Recipes.ts
@@ -0,0 +1,29 @@
+import { getEnumMember, getEnumMemberName, IngredientCategory } from "./Ingredients/IngredientTypes";
+import { IngredientInfo } from "./Ingredients/Ingredient";
+
+export type Recipes = [string, IngredientInfo[]] 
+
+export const Recipe0 : IngredientInfo[] = [
+    new IngredientInfo(1,1),    //Onion
+    new IngredientInfo(1,11),   //Garlic
+    new IngredientInfo(0,0),    //Beef
+    new IngredientInfo(3,4),    //ChickenBroth
+    new IngredientInfo(1,6),    //Tomato
+    new IngredientInfo(1,2),    //Carrot
+    new IngredientInfo(2,5)     //Rosemary
+]
+
+export const Recipe1 : IngredientInfo[] = [
+    new IngredientInfo(1,1),    //Onion
+    new IngredientInfo(1,7),    //Celery
+    new IngredientInfo(0,1),    //Chicken
+    new IngredientInfo(1,2),    //Carrot
+    new IngredientInfo(1,0),    //Potatoe
+    new IngredientInfo(4,6),    //Bay Leaf
+    new IngredientInfo(5,0)     //Noodles
+]
+
+export const ourRecipes : Recipes[] = [
+    ["BeefStew", Recipe0],
+    ["Chicken Noodle", Recipe1]
+]
