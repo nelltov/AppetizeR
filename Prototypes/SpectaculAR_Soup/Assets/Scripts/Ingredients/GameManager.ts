489d2d12fc1336f02f6028221372fecf632d2795
paulotassi1 <133156624+paulotassi@users.noreply.github.com>
2026-02-20 16:40:18 -0500

    Update GameManager.ts

--- Prototypes/SpectaculAR_Soup/Assets/Scripts/Ingredients/GameManager.ts
+++ Prototypes/SpectaculAR_Soup/Assets/Scripts/Ingredients/GameManager.ts
@@ -3,9 +3,17 @@ import {SyncEntity} from "SpectaclesSyncKit.lspkg/Core/SyncEntity";
 import {StorageProperty} from "SpectaclesSyncKit.lspkg/Core/StorageProperty"
 import {SessionController } from "SpectaclesSyncKit.lspkg/Core/SessionController"
 import { IngredientInfo } from "./Ingredient";
+import { EventManager } from "Scripts/EventManager";
+import { BaseButton } from "SpectaclesUIKit.lspkg/Scripts/Components/Button/BaseButton";
+
+export enum RoundState{
+    
+    PreRound,
+    InRound,
+    PostRound
+}
 
 type Recipes = [string, IngredientInfo[]] 
-
 
 export const Recipe0 : IngredientInfo[] = [
     new IngredientInfo(1,1),    //Onion
@@ -30,6 +38,68 @@ export const Recipe1 : IngredientInfo[] = [
 const ourRecipes : Recipes[] = [
     ["BeefStew", Recipe0],
     ["Chicken Noodle", Recipe1]
-    
 ]
 
+@component
+export class GameManager extends BaseScriptComponent {
+
+    
+
+    private syncEntity: SyncEntity
+    private playerId: number
+    private currentRecipe = StorageProperty.manualInt("currentRecipe", 1)
+    private currentChef = StorageProperty.manualString("currentChefConnectionId", "");
+
+
+
+    onReady() 
+    {
+        //Reference to the UI button to start game
+        const startGameButton = this.getSceneObject().getComponent(
+            BaseButton.getTypeName()
+        ) as BaseButton;
+
+        //Event subscribing to make a new chef
+        let startEvent = startGameButton.onStateChanged
+        startEvent.bind(() => { this.chooseRandomChef() })
+    }
+
+
+    onAwake() {
+
+        //Setting Sync Entity
+        this.syncEntity = new SyncEntity(this);
+
+        //Setting up necessary functions and subscriptions once in sessions
+        this.syncEntity.notifyOnReady(() => this.onReady())
+
+        //Initializing the Chef Variab;le
+        this.syncEntity.addStorageProperty(this.currentChef);
+    }
+    
+    chooseRandomChef()
+    {
+        //Sort through all users
+        const users = SessionController.getInstance().getUsers();
+
+        //Choose Random
+        const chosenChef = this.getRandomElement(users);
+
+        //Get connection Id from UserInfo
+        const chosenConnectionId = (chosenChef as any).connectionId as string;
+
+        //sets new Main Chef
+        this.currentChef.setPendingValue(chosenConnectionId)
+
+        print("Picked chef connectionId = " + chosenConnectionId);
+    }
+    getRandomElement<T>(array: T[]): T | undefined
+    {
+        if (array.length === 0)
+        {
+            return undefined;
+        }
+        const randomIndex = Math.floor(Math.random() * array.length);
+        return array[randomIndex];
+    }
+}
