be927dcf79b12bf6d182c4e41eb4738b626d3396
Nellie Tonev <nellie.tonev@gmail.com>
2026-02-21 00:48:14 -0500

    accessing shader properties in script

--- Prototypes/SpectaculAR_Soup/Assets/Scripts/ImageMarker/SoupStirring.ts
+++ Prototypes/SpectaculAR_Soup/Assets/Scripts/ImageMarker/SoupStirring.ts
@@ -1,10 +1,25 @@
 @component
 export class SoupStirring extends BaseScriptComponent {
     private soupMaterial: Material
+    private soupShader: Pass
 
     onAwake() {
-
-        
         // Set up Start and Update events
+        let startEvent = this.createEvent("OnStartEvent")
+        startEvent.bind(() => { this.onStart() })
+
+        let updateEvent = this.createEvent("UpdateEvent")
+        updateEvent.bind(() => { this.onUpdate() })
+    }
+
+    onStart() {
+        this.soupMaterial = this.sceneObject.getComponent("Component.RenderMeshVisual").getMaterial(0)
+        this.soupShader = this.soupMaterial.mainPass
+        // print(`Shader rotation speed - ${this.soupShader.rotationSpeed}`)
+        // print(`Shader swirl amount - ${this.soupShader.swirlAmount}`)
+    }
+
+    onUpdate() {
+
     }
 }
