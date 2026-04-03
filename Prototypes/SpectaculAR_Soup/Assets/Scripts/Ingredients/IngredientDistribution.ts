7b99d8f8f71c7eeb1930054a856106a125cb0854
Nellie Tonev <nellie.tonev@gmail.com>
2026-04-03 03:13:13 -0400

    distribute player ingredients based on recipe

--- Prototypes/SpectaculAR_Soup/Assets/Scripts/Ingredients/IngredientDistribution.ts
+++ Prototypes/SpectaculAR_Soup/Assets/Scripts/Ingredients/IngredientDistribution.ts
@@ -1,11 +1,33 @@
-import { IngredientInfo } from "./Ingredient";
+import { IngredientInfo, IngredientType } from "./Ingredient"
 
 export function distributeIngredients(recipe: IngredientInfo[], numPlayers: number): number[][] {
-    // TODO: Replace placeholder with real implementation
+    let allIngredientOptions : number[] = shuffle(Object.values(IngredientType)).filter(value => typeof value === "number") as number[]
+    let recipeIngredients : number[] = shuffle(recipe.map(ingredientInfo => ingredientInfo.ingredient))
+    let nonRecipeIngredients : number[] = shuffle(allIngredientOptions).filter(ingredient => {
+        return !recipeIngredients.some(recipeIngredient => recipeIngredient === ingredient)
+    })
+
+    const numPlates = 6
+    const platesToPopulate = numPlayers * numPlates
+
+    // First prioritize recipe ingredients, then all the unique ingredients
+    let ingredientsToDistribute = recipeIngredients.concat(nonRecipeIngredients).slice(0, platesToPopulate)
+
+    // If there are extra empty plates, keep adding ingredients (including duplicates) until all plates are filled
+    while (ingredientsToDistribute.length < platesToPopulate) {
+        ingredientsToDistribute = ingredientsToDistribute.concat(allIngredientOptions).slice(0, platesToPopulate)
+    }
+
+    // Give out ingredients in round-robin fashion
     let result : number[][] = []
     for (let i = 0; i < numPlayers; i++) {
-        result.push(shuffle([3, 4, 7, 10, 11, 15]))
+        let playerIngredients = []
+        for (let j = 0; j < numPlates; j++) {
+            playerIngredients.push(ingredientsToDistribute[j * numPlayers + i])
+        }
+        result.push(playerIngredients)
     }
+
     print("Distribution Result: " + JSON.stringify(result))
     return result
 }
