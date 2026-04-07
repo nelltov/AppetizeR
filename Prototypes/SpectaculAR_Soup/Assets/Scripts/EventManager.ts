3d3f7e2b61ac734e6bca614666fac099f27a6170
Nellie Tonev <nellie.tonev@gmail.com>
2026-04-07 11:39:41 -0400

    organize event manager

--- Prototypes/SpectaculAR_Soup/Assets/Scripts/EventManager.ts
+++ Prototypes/SpectaculAR_Soup/Assets/Scripts/EventManager.ts
@@ -7,22 +7,27 @@ import { IngredientInfo } from "./Ingredients/Ingredient";
 @component
 export class EventManager extends BaseScriptComponent {
     // Game Events
-    static CenterPositionSetLocal = new EventWrapper<[vec3]>()
-    static SpawnPlayerIngredients = new EventWrapper<[number[]]>()
+    // Put chef and apprentice objects around the table
+    static CenterPositionSetLocal = new EventWrapper<[vec3]>()  
+    static SpawnPlayerIngredients = new EventWrapper<[number[]]>()  
     static SpawnChefInstructions = new EventWrapper<[IngredientInfo[]]>()
-    static NextInstructionNetworkEvent = new EventWrapper()
+
+    // Chef progressing through recipe instructions and other players adding ingredients to soup
+    static NextInstructionNetworkEvent = new EventWrapper() 
     static NextInstructionLocalEvent = new EventWrapper()
     static SoupPotIngredientCollisionNetworkEvent = new EventWrapper<[IngredientInfo]>() 
     static SoupPotIngredientCollisionLocalEvent = new EventWrapper<[IngredientInfo]>() 
+
+    // Transition to the stage of checking the soup
     static CheckSoupLocalEvent = new EventWrapper()
     static CheckSoupNetworkEvent = new EventWrapper()
 
-    // Game States
+    // Ending and resetting the game
     static PlayerVictoryNetworkEvent = new EventWrapper<[boolean]>()
     static PlayerVictoryLocalEvent = new EventWrapper<[boolean]>()
     static ResetGameNetworkEvent = new EventWrapper()
 
-    // Shader
+    // Shader/Soup Pot actions
     static UpdateSoupSwirlAmount = new EventWrapper<[number]>()
 
     // Add more events as needed, following the pattern above.
