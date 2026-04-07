56fbe76ac1acb03c3c4bfce6321f4ea99b0cba51
paulotassi1 <133156624+paulotassi@users.noreply.github.com>
2026-04-07 12:50:37 -0400

    test

--- Prototypes/SpectaculAR_Soup/Assets/Scripts/ResultHelper.ts
+++ Prototypes/SpectaculAR_Soup/Assets/Scripts/ResultHelper.ts
@@ -2,7 +2,8 @@ import { EventManager } from "./EventManager";
 
 @component
 export class ResultHelper extends BaseScriptComponent {
-  
+  @input
+  winObject: SceneObject | null;
     onAwake(): void {
         let startEvent = this.createEvent("OnStartEvent")
         startEvent.bind(() => { this.onStart() })
@@ -15,6 +16,9 @@ export class ResultHelper extends BaseScriptComponent {
         EventManager.PlayerVictoryNetworkEvent.add((isVictory: boolean) => {
             if (isVictory) {
                 print("GM heard the Event Manager call for a victory");
+                if (this.winObject?.enabled == false){
+                    this.winObject.enabled = true;
+                }
                 visualEffect.enabled = true;
             } else {
                 print("GM heard the Event Manager call for a loss");
