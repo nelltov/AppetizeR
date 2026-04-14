509dc776867d4c7e1666344c08e2ea5a09ea05e9
Nellie Tonev <nellie.tonev@gmail.com>
2026-04-14 18:34:22 -0400

    reworked starting UX flow

--- Prototypes/SpectaculAR_Soup/Assets/Scripts/ConfirmPosition.ts
+++ Prototypes/SpectaculAR_Soup/Assets/Scripts/ConfirmPosition.ts
@@ -1,59 +1,58 @@
 import { EventManager } from "./EventManager"
 import { InteractableManipulation } from "SpectaclesInteractionKit.lspkg/Components/Interaction/InteractableManipulation/InteractableManipulation"
-import { SessionController } from "SpectaclesSyncKit.lspkg/Core/SessionController"
 import { SyncEntity } from "SpectaclesSyncKit.lspkg/Core/SyncEntity"
-import { StorageProperty } from "SpectaclesSyncKit.lspkg/Core/StorageProperty"
 
 @component
 export class ConfirmPosition extends BaseScriptComponent {
     private syncEntity: SyncEntity
-    private playersReady = StorageProperty.manualInt("playersReady", 0);
-    private totalPlayers: number
-
-    @input
-    gameManagersToEnable: SceneObject
+    private playersReady: number
+    public totalPlayers: number
 
     @input
     confirmButton: SceneObject
 
     @input
-    chefSelectionButton: SceneObject
+    startingObject: SceneObject
+    private startObjectInteractableManipulation: InteractableManipulation
 
     @input
     gameRoot: SceneObject
-    private gameRootInteractableManipulation: InteractableManipulation
+
+    // Objects to enable when all players are ready
+    @input
+    public objectsToDisable : SceneObject[]
 
     @input
-    debugText: Text
+    public objectsToEnable : SceneObject[]
 
     onAwake() {
         this.syncEntity = new SyncEntity(this);
         this.syncEntity.notifyOnReady(() => this.onReady())
-
-        this.syncEntity.addStorageProperty(this.playersReady)
     }
 
     onReady() {
-        this.totalPlayers = SessionController.getInstance().getUsers().length
-        if (this.debugText) this.debugText.text = this.debugText.text + `\nExpected Total Players: ${this.totalPlayers}`
-        this.gameRootInteractableManipulation = this.gameRoot.getComponent(InteractableManipulation.getTypeName()) as InteractableManipulation
-        
-        this.playersReady.onAnyChange.add((newVal: number) => {
-            if (this.debugText) this.debugText.text = this.debugText.text + `\nPlayers Ready: ${newVal}`
-            if (newVal === this.totalPlayers) {
-                // all players confirmed game positions
-                this.chefSelectionButton.enabled = true
-                this.gameManagersToEnable.enabled = true
-                EventManager.CenterPositionSetLocal.trigger(this.gameRoot.getTransform().getWorldPosition());
-            }
+        this.startObjectInteractableManipulation = this.startingObject.getComponent(InteractableManipulation.getTypeName()) as InteractableManipulation
+
+        this.playersReady = 0
+        this.syncEntity.onEventReceived.add("playerIsReady", () => {
+            this.playersReady += 1
         })
+    }
+
+    public checkAllPlayersReady() {
+        if (this.playersReady === this.totalPlayers) {
+            this.objectsToEnable.forEach((obj) => obj.enabled = true)
+            this.objectsToDisable.forEach((obj) => obj.enabled = false)
+
+            EventManager.CenterPositionSetLocal.trigger(this.gameRoot.getTransform().getWorldPosition())
+        }
     }
 
     /* Lock position and deactivate button for each individual player */
     public confirmGamePosition() {
-        const updatedPlayersReady = this.playersReady.currentOrPendingValue + 1
+        print("confirm game position")
         this.confirmButton.enabled = false
-        this.gameRootInteractableManipulation.enabled = false
-        this.playersReady.setPendingValue(updatedPlayersReady)
+        this.startObjectInteractableManipulation.enabled = false
+        this.gameRoot.enabled = true
     }
 }
--- Prototypes/SpectaculAR_Soup/Assets/Scripts/InstructionManager.ts
+++ Prototypes/SpectaculAR_Soup/Assets/Scripts/InstructionManager.ts
@@ -1,3 +1,7 @@
+import { SessionController } from "SpectaclesSyncKit.lspkg/Core/SessionController"
+import { SyncEntity } from "SpectaclesSyncKit.lspkg/Core/SyncEntity"
+import { ConfirmPosition } from "./ConfirmPosition"
+
 @component
 export class InstructionManager extends BaseScriptComponent {
 
@@ -8,20 +12,49 @@ export class InstructionManager extends BaseScriptComponent {
     public instructionStringHolderObject : Text
 
     @input
-    public saltShakerObject : SceneObject
+    public objectsToDisable : SceneObject[]
 
     @input
-    public gameRootObject : SceneObject
+    public objectsToEnable : SceneObject[]
+
+    @input
+    confirmPositionObject: SceneObject
+    private confirmPositionSyncEntity: SyncEntity
 
     private currentInstruction: number = 0
 
-    public nextInstruction()
-    {
+    onAwake() {
+        let startEvent = this.createEvent("OnStartEvent")
+        startEvent.bind(() => { this.onStart() })
+    }
+
+    onStart() {
+        this.confirmPositionSyncEntity = SyncEntity.getSyncEntityOnSceneObject(this.confirmPositionObject)
+        const totalPlayers = SessionController.getInstance().getUsers().length
+
+        // Set total player count in the ConfirmPosition script
+        const confirmPositionScript = this.confirmPositionObject.getComponent(
+            ConfirmPosition.getTypeName()
+        ) as ConfirmPosition
+        if (confirmPositionScript) confirmPositionScript.totalPlayers = totalPlayers
+
+
+        // Bind function to check if all players are ready after setting total player count
+        this.confirmPositionSyncEntity.onEventReceived.add("checkAllPlayersReady", () => {
+            confirmPositionScript.checkAllPlayersReady()
+        })
+    }
+
+    public nextInstruction() {
         this.currentInstruction += 1
 
         if (this.currentInstruction >= this.instructionArray.length) {
-            this.saltShakerObject.enabled = false
-            this.gameRootObject.enabled = true
+            this.objectsToEnable.forEach((obj) => obj.enabled = true)
+            this.objectsToDisable.forEach((obj) => obj.enabled = false)
+            
+            // Send events indicating that a player is ready
+            this.confirmPositionSyncEntity.sendEvent("playerIsReady", {})
+            this.confirmPositionSyncEntity.sendEvent("checkAllPlayersReady", {})
         } else {
             this.instructionStringHolderObject.text= this.instructionArray[this.currentInstruction]
         }
