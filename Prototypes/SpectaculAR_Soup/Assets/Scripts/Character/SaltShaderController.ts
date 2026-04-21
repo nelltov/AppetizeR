cbe10dad9b5a6a0d42673c4ed238b191ee87179c
paulotassi1 <133156624+paulotassi@users.noreply.github.com>
2026-04-21 17:21:38 -0400

    added random interval changes for heart eyes

--- Prototypes/SpectaculAR_Soup/Assets/Scripts/Character/SaltShaderController.ts
+++ Prototypes/SpectaculAR_Soup/Assets/Scripts/Character/SaltShaderController.ts
@@ -1,5 +1,6 @@
 import { EventManager } from "Scripts/EventManager"
 import Event from "SpectaclesInteractionKit.lspkg/Utils/Event"
+import { setTimeout } from "SpectaclesInteractionKit.lspkg/Utils/FunctionTimingUtils"
 
 @component
 export class SaltShaderController extends BaseScriptComponent {
@@ -21,6 +22,10 @@ export class SaltShaderController extends BaseScriptComponent {
 
     private tilt: number
     private pour: boolean
+
+    public cooldownHeart: number = 1;
+    private startHeartTime: number = 0;
+    private currentHeartDuration: number = 0;
 
     onAwake() {
         let startEvent = this.createEvent("OnStartEvent")
@@ -49,6 +54,11 @@ export class SaltShaderController extends BaseScriptComponent {
         // Shader params
         this.saltShader.surfacelevel = this.calculateSurfaceLevel(this.wh, this.tilt, this.saltAmount)
 
+        EventManager.SoupPotIngredientCollisionNetworkEvent.add(() => {
+           this.startHeartTime = getTime();
+           this.cooldownHeart = Math.random() + 0.5;
+           print("Heard the ingredient collide. StartHeartTime is: " + this.startHeartTime);
+        })
         // Reset saltie amount
         EventManager.ResetGameNetworkEvent.add(() => {
             this.saltAmount = 0.8
@@ -59,6 +69,7 @@ export class SaltShaderController extends BaseScriptComponent {
     onUpdate() {
         let transform = this.sceneObject.getTransform()
 
+        this.currentHeartDuration = getTime();
         // tilting status
         this.tilt = transform.up.dot(new vec3(0, 1, 0))
         this.pour = this.tilt < this.pourThreshold
@@ -69,6 +80,14 @@ export class SaltShaderController extends BaseScriptComponent {
         if (this.pour) {
             this.saltAmount = Math.max(this.saltAmount - this.pourSpeed * 0.01, 0.01)
         } 
+
+        if (this.startHeartTime + this.cooldownHeart >= this.currentHeartDuration){
+            print("Should change the face to hearts");
+            this.faceShader.love = true;
+        }
+        else {
+            this.faceShader.love = false;
+        }
 
         // Pour VFX
         if (this.saltVFX) this.saltVFX.enabled = this.pour && this.saltAmount > 0.03
